63435e0fdf0ff6220f4e94b4fcd1234f054eedc1
Noa B <noaboserup@gmail.com>
2025-12-15 13:30:37 +0100

    Fixed grid

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ running = True
 
 def grid(gridsize, gridamount):
     top = screenwidth / 16
-    left = screenheight / 8
+    left = screenheight * 6/8 
     gridspacing = gridsize/gridamount
     for x in range(gridamount):
         for y in range(gridamount):
-            gridsquare = pg.Rect(gridspacing*x + top, gridspacing*y + left, gridspacing, gridspacing)
+            gridsquare = pg.Rect(gridspacing*x + left + 5*x, gridspacing*y + top +5*y, gridspacing, gridspacing)
             pg.draw.rect(screen, (50, 50, 255), gridsquare)
 
 while running:
